24717937a89d12a61a515d5f99e9204758972fab
Alex Fogal <afogal@users.noreply.github.com>
2020-06-08 15:44:34 -0400

    Major overhauls
    Added a 3D square lattice random walk model, made some changes to the 2D one which is considered totally inactive at this point. Added a script to turn the sequences of images into a video.

--- folding_2d.py
+++ folding_2d.py
@@ -9,7 +9,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 # Some Variables to control number of amino acids and the dimensions of the grid, temperature, Boltzmann constant
 num_acids = 20
-dim = int(num_acids * 1.5)
+dim = num_acids *3
 T=10
 kb = 1.38e-23
 animate = True
@@ -17,14 +17,14 @@ maxfolds=10000
 
 now = datetime.now()
 datetime = now.strftime("%m-%d-%Y_%H%M%S")
-genericname = datetime + "_L{}_T{}_F{}_3D".format(num_acids,T,maxfolds)
+genericname = datetime + "_L{}_T{}_F{}_2D".format(num_acids,T,maxfolds)
 os.mkdir("pics-{}".format(genericname))
 
-print("3D Time: {} Folds: {} Length: {} Temp: {}".format(datetime, maxfolds, num_acids, T))
+print("2D: Time: {} Folds: {} Length: {} Temp: {}".format(datetime, maxfolds, num_acids, T))
 
 # Storage variables
 A = np.zeros((num_acids))
-Axyz = np.zeros((num_acids, 3))
+Axy = np.zeros((num_acids, 2))
 totalE = list()
 
 # TODO: start folding and then tack on more acids as its in the middle of folding
@@ -32,7 +32,7 @@ totalE = list()
 # Generate the primary structure and give each acid coords
 for i in range(len(A)):
     A[i]=random.randint(0,19)
-    Axyz[i]=[i, 0, 0]
+    Axy[i]=[i, 0]
     
 #Matrix of interaction energies
 #Taken from: doi:10.1016/j.jmb.2005.01.071
@@ -61,12 +61,7 @@ for i in range(len(A)):
 J = np.zeros((20,20))
 for i in range(0,20):
     for j in range(0,20):
-        J[i][j] = random.randint(-1,1)
-
-# Random polar vs nonpolar AAs
-Jw = np.zeros((20,))
-for i in range(0,20):
-    Jw[i] = list([-5,5,-1,1])[random.randint(0,3)] # each is either strongly P/NP or slightly P/NP, numbers were chosen st PvsNP is the driving folder as opposed to interacitons bw AAs
+        J[i][j] = random.randint(-5,-1)
 
 #Are A1 and A2 nearest neighours?
 def NN(A1, A2):
@@ -78,29 +73,19 @@ def NN(A1, A2):
     
     #np.all ensures the arrays are exactly equal
     #acids are nearest neighbours if their distance is exactly 1, since we are using a cubic lattice, this means that the x,y, or z must be exactly one off
-    if(np.all(dA == [1,0,0]) or np.all(dA == [0,1,0]) or np.all(dA == [0,0,1])):
+    if(np.all(dA == [1,0]) or np.all(dA == [0,1])):
         areNN=True
     
     return areNN
     
 #Find the energy of the current chain
-wcnt = 0
-def calcE(A,Axyz):
-    global wcnt
+def calcE(A,Axy):
     E = 0
     for i in range(len(A)):
         for j in range(len(A)):
             #are i and j nearest neighbours and not bonded?
-            if(not (np.abs(i-j) in [0,1]) and NN(Axyz[i], Axyz[j])):
+            if(not (np.abs(i-j) in [0,1]) and NN(Axy[i], Axy[j])):
                 E += J[int(A[i])][int(A[j])]
-
-
-    for i in range(len(A)):
-        for dir in [[1,0,0],[-1,0,0],[0,1,0],[0,-1,0],[0,0,1],[0,0,-1]]:
-            if [0,0,0] in (Axyz - (Axyz[i]+dir)):
-                E += Jw[int(A[i])]
-                wcnt += 1
-
     return E
 
 #Some variables to keep track of events
@@ -109,14 +94,14 @@ rnnfail=0
 lnnfail=0
 efail=0
 nallowed=0
-totalE.append(calcE(A,Axyz))
+totalE.append(calcE(A,Axy))
 lastmove=0
 try:
     while(nallowed<maxfolds):
         n+=1 #step counter
         if(n-lastmove > 50000):
             break
-        
+
         if(nallowed > 0.3*maxfolds and nallowed < 0.8*maxfolds):
             T=4        
         if(nallowed > 0.8*maxfolds and nallowed < 0.9*maxfolds):
@@ -124,67 +109,70 @@ try:
         if(nallowed > 0.9*maxfolds):
             T=1        
         
+
+
         #calculate E1
-        E1 = calcE(A,Axyz)
+        E1 = calcE(A,Axy)
         
         #generate move
-        moves = [[1,1,0],[0,1,1],[1,0,1],[-1,1,0],[0,-1,1],[-1,0,1],[1,-1,0],[0,1,-1],[1,0,-1],[-1,-1,0],[0,-1,-1],[-1,0,-1]]
+        moves = [[1,1],[-1,1],[1,-1],[-1,-1]]
         
         Ai = random.randint(0, len(A)-1) #pick random acid
-        Af = Axyz[Ai] + moves[random.randint(0,11)] #pick a random move
+        Af = Axy[Ai] + moves[random.randint(0,3)] #pick a random move
         
         #test if move allowed
         allowed = True
         
         #Af must be nearest neighbours with both its preceeding and succeeding acid to not break the peptide bonds
         if(not Ai==0):
-            if(not NN(Af,Axyz[Ai-1])):
+            if(not NN(Af,Axy[Ai-1])):
                 allowed = False
                 lnnfail+=1
         if(not Ai==len(A)-1):
-            if(not NN(Af,Axyz[Ai+1])):
+            if(not NN(Af,Axy[Ai+1])):
                 allowed = False
                 rnnfail+=1
         
         #Af must not be an already occupied position
         for i in range(len(A)):
-            if(np.all(Af == Axyz[i])):
+            if(np.all(Af == Axy[i])):
                 allowed = False
                 efail+=1
         
         #calculate E2 and deltaE
         if(allowed):
-            newAxyz = Axyz.copy()
-            newAxyz[Ai] = Af
-            E2 = calcE(A, newAxyz)
+            newAxy = Axy.copy()
+            newAxy[Ai] = Af
+            E2 = calcE(A, newAxy)
             deltaE = E2-E1
         else:
             deltaE = 0
-            newAxyz = Axyz.copy()
+            newAxy = Axy.copy()
 
         
         #move if deltaE is negative or less than a random boltzmann energy
         bE = np.exp((-1 * deltaE)/(T))
         if(allowed and (deltaE <= 0 or random.uniform(0,1) < bE)):
             lastmove=n
-            Axyz = newAxyz.copy()
+            Axy = newAxy.copy()
             nallowed+=1
             totalE.append(E2)
             
             if(animate):
                 fig = plt.figure(figsize=(4,4), dpi=600)
-                ax = fig.add_subplot(111, projection='3d')
-                ax.set(xlim=(-1, len(A)), ylim=(-5, 5), zlim=(-5,5))
-
+                ax = plt.axes()
+                ax.set(xlim=(-1, len(A)), ylim=(-5, 5))
+                
                 for i in range(1,len(A)):
-                    dx = np.linspace(newAxyz[i-1][0], newAxyz[i][0], 1000)
-                    dy = np.linspace(newAxyz[i-1][1], newAxyz[i][1], 1000)
-                    dz = np.linspace(newAxyz[i-1][2], newAxyz[i][2], 1000)
-                    ax.scatter(dx,dy,dz,c='k')
+                    dx = np.linspace(newAxy[i-1][0], newAxy[i][0], 1000)
+                    dy = np.linspace(newAxy[i-1][1], newAxy[i][1], 1000)
+                    ax.scatter(dx,dy,c='k')
 
-                ax.scatter([b[0] for b in newAxyz],[b[1] for b in newAxyz],[b[2] for b in newAxyz],c='r')
-                graphtext = "Fold: {}, T={}".format(nallowed, T)
-                ax.text2D(0.2, 0.9, graphtext, horizontalalignment='center', verticalalignment='center', transform = ax.transAxes)
+                ax.scatter([b[0] for b in newAxy],[b[1] for b in newAxy],c='r')
+                plt.text(0.2, 0.9,'Fold: {}, T={}'.format(nallowed, T),
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    transform = ax.transAxes)
 
 
                 name="pics-{}/fold-{:07d}.png".format(genericname,nallowed)
@@ -196,7 +184,6 @@ except:
 # Summary
 print("Number of overlapping states denied: " + str(efail))
 print("Number of bond stretches denied: {} (right) {} (left)".format(rnnfail,lnnfail))
-print("Number of water interactions: {}".format(wcnt))
 print("Number of folds made: " + str(nallowed))
 print("Number of steps: " + str(n))
 
